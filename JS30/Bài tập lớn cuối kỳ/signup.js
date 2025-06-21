c3efcb87016d89c47fb56e1eca538db7a1dd3018
NguyenDuyTu275 <tunguyenduy.ptit@gmail.com>
2025-06-21 08:45:09 +0700

    bài thực hành ngày 20/6/2025

--- JS30/Bài tập lớn cuối kỳ/signup.js
+++ JS30/Bài tập lớn cuối kỳ/signup.js
@@ -6,18 +6,4 @@ if (localStorage.getItem('signup')==='true'){
     window.location.href="trang2.html";
 }
 
-class user{
-    username;
-    password;
-    mail;
-}
-a=new user;
-if (pass===test){
-    localStorage.setItem('signup','true');
-    window.location.href='trang2.html';
-    a.username=user;
-    a.password=pass;
-    a.mail=email;
-    localStorage.setItem('1',a);    
-}
-b=console.log('có lấy dữ liệu'+localStorage.getItem('1'));
+
